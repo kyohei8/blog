39aa5f385fcdddbc5241ed6e9850a37115d0ab84
kyohei tsukuda <tsukuda.kyouhei@gmail.com>
2020-07-30 15:24:58 +0900

    fixed layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,11 @@ import Footer from '../components/footer';
 
 export default ({ Component, pageProps }) => (
   <>
-    <div className="mx-auto max-w-3xl pt-6 px-3">
-      <Component {...pageProps} />
+    <div className="flex flex-col min-h-screen">
+      <div className="mx-auto max-w-3xl pt-6 px-3 flex-grow">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
-    <Footer />
   </>
 );
