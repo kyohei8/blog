1b6fe85a2db4aba23eb9dfba2305c84b59b2e7a8
Kyohei Tsukuda <tsukuda.kyouhei@gmail.com>
2022-01-30 17:57:42 +0900

    Fixed Image modal

--- src/components/imageModal.tsx
+++ src/components/imageModal.tsx
@@ -23,53 +23,21 @@ const ImageModal: React.FC<ImageModalProps> = ({ bindings, imageSrc }) => {
       style={{
         maxHeight: 'calc(100vh - 40px)',
         minHeight: '50px',
-        borderRadius: '4px',
+        borderRadius: 0,
         padding: 0,
         backgroundColor: '#ccc'
       }}
     >
-      <StyledWrapper
-        // style={{ backgroundImage: `url(${imageSrc})` }}
-        onClick={e => {
-          e.stopPropagation();
-        }}
-      >
+      <StyledWrapper>
         <Loading css={{ position: 'absolute', padding: '$4' }} />
         <img src={imageSrc} style={{ position: 'relative' }} />
       </StyledWrapper>
-      {/*
-  <div className="image-modal z-20 fixed top-0 left-0 w-full h-full flex justify-center items-center select-none">
-    <div className="absolute right-0 top-0 text-white">
-      <span
-        className="inline-block p-4 cursor-pointer"
-        onClick={() => {
-          onClose();
-        }}
-      >
-        <X size={32} />
-      </span>
-    </div>
-    <div
-      className="w-full h-full py-16"
-      onClick={() => {
-        onClose();
-      }}
-    >
-      <div
-        className="w-full h-full bg-no-repeat bg-center bg-contain"
-        style={{ backgroundImage: `url(${imageSrc})` }}
-        onClick={e => {
-          e.stopPropagation();
-        }}
-      />
-    </div>
-  </div>
-      */}
     </Modal>
   );
 };
 
 const StyledWrapper = styled('div', {
+  fontSize: 0,
   width: '100%',
   height: '100%',
   backgroundRepeat: 'no-repeat',
